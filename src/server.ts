12846529be7fca0aceb6b348797df53c658f9dfc
Eden Lu <edenlu@microsoft.com>
2024-06-25 04:40:00 -0700

    Update cors

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
-  "https://signin-doctor.azurewebsites.net/",
+  "https://signin-doctor.azurewebsites.net",
 ];
 
 app.use(bodyParser.json());
