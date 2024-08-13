9d5f5287fe2b4c076ce7021077b290422c142c35
Eden Lu <edenlu@microsoft.com>
2024-08-12 18:29:02 -0700

    update installation script again

--- src/diagnoseSignIn.ts
+++ src/diagnoseSignIn.ts
@@ -9,7 +9,7 @@ export const diagnoseSignIn = async (
 ) => {
   const browser = await puppeteer.launch({
     headless: true,
-    executablePath: executablePath,
+    executablePath,
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
   const page = await browser.newPage();
