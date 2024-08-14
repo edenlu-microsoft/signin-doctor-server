027bdd784950803f6536de220948fbb18887d3ae
Eden Lu <edenlu@microsoft.com>
2024-08-13 18:07:26 -0700

    update style

--- src/getEcomWebConfig.ts
+++ src/getEcomWebConfig.ts
@@ -3,18 +3,20 @@ import { PuppeteerClusterController } from "./PuppeteerClusterController";
 export const getEcomWebConfig = async (url: string) => {
   const cluster = PuppeteerClusterController.getInstance().getCluster();
 
-  const requestContext = await cluster.execute(url, async ({ page, data: url }) => {
-    await page.goto(url);
+  const requestContext = await cluster.execute(
+    url,
+    async ({ page, data: url }) => {
+      await page.goto(url);
 
-    // Execute the command in the console
-    const requestContext = await page.evaluate(() => {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      return (window as any).___initialData___.requestContext;
-    });
+      // Execute the command in the console
+      const requestContext = await page.evaluate(() => {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        return (window as any).___initialData___.requestContext;
+      });
 
-    return requestContext;
-  });
-
+      return requestContext;
+    }
+  );
 
   return requestContext;
 };
